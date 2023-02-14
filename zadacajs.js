ea997465a45c40f7355dfd6dbd0177397dc71447
zdravko12 <zdravkoena@yahoo.com>
2023-02-14 19:54:49 +0100

    HomeWorkLecture9

---
+++ zadacajs.js
@@ -0,0 +1,24 @@
+let name = "Zdravko Petkovski";
+let age = 28;
+let job = "Bank"
+let gender = "male"
+let livesInSkopje = true;
+
+
+let person = {
+    name:"Zdravko Petkovski",
+    age: "28",
+    job: "bank",
+    gender: "male",
+    liveInSkopje: true,
+    
+
+}
+// console.log(person);
+
+alert(person.name + " " + person.age + " "  + person.job + " " + person.gender + " " + person.liveInSkopje)
+
+function person1(){
+    alert(person.name + " " + person.age + " "  + person.job + " " + person.gender + " " + person.liveInSkopje);
+    
+}
